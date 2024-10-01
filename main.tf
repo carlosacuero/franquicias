fc490ad049e64d1cec3d4fa8b17d1e5e2ac242e9
carlosacuero <cueroc@globalhitss.com>
2024-10-01 09:49:06 -0500

    add main terraform
    se agrega la configuración de terraform

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "dbfranquicia" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql8.0"
+}
